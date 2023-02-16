d259b294e174766294b5eeef3768cbccc1c4f984
Yusuf <yusf.mbk@gmail.com>
2023-02-16 18:25:29 +0000

    adding fun fact character info

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,7 @@ var chosenImg = document.getElementById("abc");
 
 cartmanImg.addEventListener("click", () => {
 cardText.textContent =
-"Text for Cartman";
+"Cartman is loosely based on a high school classmate of Trey Parker, one of South Park’s co-creators. The creators believe that children have no social etiquette or tact, and are generally awful, so Cartman was created as a version of the worst child they could imagine. Cartman was originally called Kenny, as seen in the South Park creators’ early animated shorts. All four of the main “boys” can be seen in the shorts, but when “Kenny” dies, it’s clearly the same character that later becomes Cartman!";
 chosenImg.src ="../South-Park-Fan-Page/assets/pictures/cartman.png"
 });
 
